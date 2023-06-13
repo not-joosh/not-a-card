65a8a77c491d8868b85ad7bf8e468b1b559a7da5
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-06-13 13:24:46 +0800

    Fixed switching modes.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const body = document.body;
 const darkLightModeBtn = document.querySelector('.dark-light-mode');
 
-darkLightModeBtn.addEventListener('click', () => {
+function toggleMode() {
+    const newMode = body.classList.contains('dark') ? 'light' : 'dark';
     body.classList.toggle('dark');
     darkLightModeBtn.classList.toggle('dark');
     darkLightModeBtn.style.borderRadius = "100%";
@@ -9,4 +10,25 @@ darkLightModeBtn.addEventListener('click', () => {
         ? 'linear-gradient(90deg, #7481f8, #e574ef)'
         : 'linear-gradient(90deg, #d53369, #cbad6d)';
     body.style.background = gradient;
-});
+    localStorage.setItem('mode', newMode);
+};
+
+function initializeMode() {
+    const storedMode = localStorage.getItem('mode');
+    
+    if (storedMode === 'dark') {
+        body.classList.add('dark');
+        darkLightModeBtn.classList.add('dark');
+        darkLightModeBtn.style.borderRadius = "100%";
+        body.style.background = 'linear-gradient(90deg, #7481f8, #e574ef)';
+    } else {
+        body.classList.remove('dark');
+        darkLightModeBtn.classList.remove('dark');
+        darkLightModeBtn.style.borderRadius = "initial";
+        body.style.background = 'linear-gradient(90deg, #d53369, #cbad6d)';
+    }
+};
+
+darkLightModeBtn.addEventListener('click', toggleMode);
+
+initializeMode();   
